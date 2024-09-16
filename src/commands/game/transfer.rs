51b761f49d88d59843a8a1daa22b331c7deacf4f
Drowrin <drowrin@gmail.com>
2024-09-15 22:35:21 -0400

    add confirmation modal to transfer command

--- src/commands/game/transfer.rs
+++ src/commands/game/transfer.rs
@@ -1,7 +1,10 @@
 use serenity::all::{Member, Mentionable};
 use sqlx::query;
 
-use crate::{commands::game::can_manage, Context, Result};
+use crate::{
+    commands::{confirmation_modal, game::can_manage},
+    Context, Result,
+};
 
 /// Transfer ownership of this game to another user. Usable by game owners and server moderators.
 #[poise::command(slash_command)]
@@ -15,9 +18,9 @@ pub async fn transfer(
 ) -> Result<()> {
     can_manage(ctx, game).await?;
 
-    let role_id = query!(
+    let game_data = query!(
         r#"
-        select role_id
+        select role_id, abbreviation
         from games
         where guild_id = $1 and id = $2
         "#,
@@ -25,65 +28,75 @@ pub async fn transfer(
         game,
     )
     .fetch_one(&ctx.data().pool)
-    .await?
-    .role_id;
+    .await?;
 
-    if also_leave.unwrap_or_default() {
-        ctx.author_member()
-            .await
-            .unwrap()
-            .remove_role(ctx, role_id as u64)
+    confirmation_modal()
+        .ctx(&ctx)
+        .phrase(&game_data.abbreviation)
+        .failure_message("Confirmation failed. Game was not deleted.")
+        .success_message("Game deleted!")
+        .then(|| async {
+            if also_leave.unwrap_or_default() {
+                ctx.author_member()
+                    .await
+                    .unwrap()
+                    .remove_role(ctx, game_data.role_id as u64)
+                    .await?;
+            } else {
+                query!(
+                    r#"
+                    insert
+                    into players
+                        (game_id, user_id)
+                    values
+                        ($1, (select owner_id from games where id = $1))
+                    "#,
+                    game,
+                )
+                .execute(&ctx.data().pool)
+                .await?;
+            }
+
+            let record = query!(
+                r#"
+                update games
+                set
+                    owner_id = $3
+                where guild_id = $1 and id = $2
+                returning title
+                "#,
+                ctx.guild_id().unwrap().get() as i64,
+                game,
+                user.user.id.get() as i64,
+            )
+            .fetch_one(&ctx.data().pool)
             .await?;
-    } else {
-        query!(
-            r#"
-            insert
-            into players
-                (game_id, user_id)
-            values
-                ($1, (select owner_id from games where id = $1))
-            "#,
-            game,
-        )
-        .execute(&ctx.data().pool)
+
+            query!(
+                r#"
+                delete
+                from players
+                where game_id = $1 and user_id = $2
+                "#,
+                game,
+                user.user.id.get() as i64,
+            )
+            .execute(&ctx.data().pool)
+            .await?;
+
+            user.add_role(ctx, game_data.role_id as u64).await?;
+
+            ctx.say(format!(
+                "Transferred `{}` to {}",
+                record.title,
+                user.mention()
+            ))
+            .await?;
+
+            Ok(())
+        })
+        .call()
         .await?;
-    }
-
-    let record = query!(
-        r#"
-        update games
-        set
-            owner_id = $3
-        where guild_id = $1 and id = $2
-        returning title
-        "#,
-        ctx.guild_id().unwrap().get() as i64,
-        game,
-        user.user.id.get() as i64,
-    )
-    .fetch_one(&ctx.data().pool)
-    .await?;
-
-    query!(
-        r#"
-        delete
-        from players
-        where game_id = $1 and user_id = $2
-        "#,
-        game,
-        user.user.id.get() as i64,
-    )
-    .execute(&ctx.data().pool)
-    .await?;
-
-    user.add_role(ctx, role_id as u64).await?;
-
-    ctx.say(format!(
-        "Transferred `{}` to {}",
-        record.title,
-        user.mention()
-    ))
-    .await?;
 
     Ok(())
 }
